99f79af75ce57f6e9a21e7511dfaf0a046e0abe8
Rich Harris <hello@rich-harris.dev>
2023-02-20 15:28:13 -0500

    log headers

--- src/routes/edge/+page.server.js
+++ src/routes/edge/+page.server.js
@@ -5,6 +5,8 @@ export function load(event) {
 	const was_cold = cold;
 	cold = false;
 
+	console.log(Object.fromEntries(event.request.headers))
+
 	return {
 		ip: event.getClientAddress(),
 		city: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-city'))),
