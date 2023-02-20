200437018f9ca11200c7d6c7a0b47a5bec639387
Rich Harris <hello@rich-harris.dev>
2023-02-20 15:39:08 -0500

    tweaks

--- src/routes/node/+page.server.js
+++ src/routes/node/+page.server.js
@@ -5,9 +5,12 @@ export function load(event) {
 	const was_cold = cold;
 	cold = false;
 
+	const ip = event.getClientAddress();
+	const city = decodeURIComponent(event.request.headers.get('x-vercel-ip-city') ?? 'unknown');
+
 	return {
-		ip: event.getClientAddress(),
-		city: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-city'))),
+		ip,
+		city,
 		now: new Date().toISOString(),
 		cold: was_cold
 	};
